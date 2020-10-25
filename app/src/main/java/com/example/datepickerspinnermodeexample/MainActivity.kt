025fb0d1a0990dfa33fdbb481827abe6c79ce68f
tk-zzz <49810315+taka-z@users.noreply.github.com>
2020-10-25 23:19:54 +0900

    DatePicker(SpinnerMode)実装

--- app/src/main/java/com/example/datepickerspinnermodeexample/MainActivity.kt
+++ app/src/main/java/com/example/datepickerspinnermodeexample/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.datepickerspinnermodeexample
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            SpinnerDatePickerDialogFragment().show(supportFragmentManager, "")
+        }
+    }
+
+
+    class SpinnerDatePickerDialogFragment: DialogFragment() {
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return DatePickerDialog(requireContext(), R.style.MySpinnerDatePickerStyle,
+                    DatePickerDialog.OnDateSetListener { p0, p1, p2, p3 ->  },
+                    1,0,1)
+        }
     }
 }
